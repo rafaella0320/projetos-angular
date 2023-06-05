b3fb284293465eb7d389816b9431989137949aaf
Rafaella Moreira <rafaelladzn@gmail.com>
2023-06-05 11:46:12 -0300

    aula7

--- api-rast/src/app/app-routing.module.ts
+++ api-rast/src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PainelComponent } from './painel/painel.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-PainelComponent
-HomeComponent
-LoginComponent
 
-const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'adm12751', component: LoginComponent},
-  {path: 'painel', component: PainelComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- api-rast/src/app/app.module.ts
+++ api-rast/src/app/app.module.ts
@@ -8,19 +8,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ServicosService } from './service/servicos.service';
 import { AlunosComponent } from './alunos/alunos.component';
-import { LoginComponent } from './login/login.component';
-import { PainelComponent } from './painel/painel.component';
-import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlunosComponent,
-    LoginComponent,
-    PainelComponent,
-    HomeComponent
+    AlunosComponent
   ],
   imports: [
     BrowserModule,
